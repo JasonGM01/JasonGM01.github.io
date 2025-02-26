5fe2603f71a90e756a6017155e4f18791d0d845a
Rozenkaiser <162739345+Rozenkaiser@users.noreply.github.com>
2025-02-26 12:28:40 -0800

    message

---
+++ my-portfolio/src/app/components/resume/honors.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Honors(){
+    return(
+        <div className="p-1">
+        <h5 className="mb-2 text-m font-bold tracking-tight">Honors</h5>
+
+        </div>
+    );
+}
---
+++ my-portfolio/src/app/components/resume/skills.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Skills(){
+    return(
+        <div className="p-1">
+        <h5 className="mb-2 text-m font-bold tracking-tight">Skills</h5>
+
+        </div>
+    );
+}
