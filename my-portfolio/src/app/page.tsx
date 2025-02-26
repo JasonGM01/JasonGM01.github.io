6805cbf2d89040db9bc74af962c3f98947cfe1d9
Rozenkaiser <162739345+Rozenkaiser@users.noreply.github.com>
2025-02-26 11:40:33 -0800

    initial commit

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Education from "@/app/components/resume/education";
+
+export default function Home() {
+  return (
+    <div className="h-screen p-4 lg:p-8 mt-16">
+      <Education />
+    </div>
+  );
+}
