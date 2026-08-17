06e358e1904e39534f08f55d1fcb9bdcff03b046
JasonGM01 <jakisakilla@gmail.com>
2026-08-17 15:35:44 -0700

    Restore portfolio code

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// @ts-ignore: CSS module import handled by Next.js
 import "./globals.css";
 
 const geistSans = Geist({
